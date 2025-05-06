70baaa91b79117c6783bd3a51b2226c4bfe9e86b
ClaireVanlerberghe <vanlerberghe24@gmail.com>
2025-05-06 21:49:59 +0200

    header ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tidyMinds';
+  showHeader: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: any) => {
+      if (event.url) {
+        this.checkRoute(event.url);
+      }
+    });
+   }
+
+   //N'affiche pas le header sur les pages login et signup
+   checkRoute(url: string) {
+    const noHeaderRoutes = ['/login', '/signup'];
+    this.showHeader = !noHeaderRoutes.includes(url);
+  }
 }
