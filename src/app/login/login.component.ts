b3814bd78bf62813e769075c07401e5a2d5ab105
ClaireVanlerberghe <vanlerberghe24@gmail.com>
2025-05-30 18:56:34 +0200

    visuel page connexion ok

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  colors: any[] = []
+  
+  form = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  login() {
+    
+  }
 
 }
